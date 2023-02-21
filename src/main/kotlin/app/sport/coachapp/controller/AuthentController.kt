3ea354afa10bc26beb2c4728e646bf3cbff14610
PC\apouz <a.pouzelgues@gmail.com>
2023-02-21 07:28:59 +0100

    Remove authentication with token

--- src/main/kotlin/app/sport/coachapp/controller/AuthentController.kt
+++ src/main/kotlin/app/sport/coachapp/controller/AuthentController.kt
@@ -3,6 +3,7 @@ package app.sport.coachapp.controller
 import app.sport.coachapp.domain.AuthentService
 import app.sport.coachapp.entities.Login
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,7 +15,14 @@ class AuthentController {
     private lateinit var authentService : AuthentService
 
     @PostMapping("")
-    fun authenticate(@RequestBody credentials: Login) : Long {
-        return authentService.authenticate(credentials)
+    fun login(@RequestBody credentials: Login) : ResponseEntity<String> {
+
+        var userId = authentService.login(credentials)
+
+        if (userId == -1L) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User unautorized");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body("{ \"id\":$userId }")
+        }
     }
 }
--- src/main/kotlin/app/sport/coachapp/domain/AuthentService.kt
+++ src/main/kotlin/app/sport/coachapp/domain/AuthentService.kt
@@ -3,15 +3,9 @@ package app.sport.coachapp.domain
 import app.sport.coachapp.entities.Login
 import app.sport.coachapp.repositories.IUserRepository
 import app.sport.coachapp.util.PasswordUtil
-import com.nimbusds.jose.JWSAlgorithm
-import com.nimbusds.jose.JWSHeader
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.oauth2.jwt.JwtClaimsSet
-import org.springframework.security.oauth2.jwt.JwtEncoder
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters
 import org.springframework.stereotype.Service
-import java.time.Instant
-import java.time.temporal.ChronoUnit
+
 
 @Service
 class AuthentService() {
@@ -19,14 +13,9 @@ class AuthentService() {
     @Autowired
     private lateinit var userRepository: IUserRepository
 
-    fun authenticate(body: Login) : Long {
+    fun login(body: Login) : Long {
 
-        var user = userRepository.findByEmail(body.email)
-
-        if (user == null || !PasswordUtil.checkPassword(body.password, user.password)) {
-            -1;
-        } else {
-            return user.id;
-        }
+        val user = userRepository.findByEmail(body.email)
+        return if (!PasswordUtil.checkPassword(body.password, user.password)) -1 else user.id;
     }
 }
